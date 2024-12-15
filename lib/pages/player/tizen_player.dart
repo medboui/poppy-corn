d2d822aeea8f3b8f92a186fa8ce5116fe2b67984
unknown <bouichou17@gmail.com>
2024-12-14 22:55:25 -0500

    add subtitles

--- lib/pages/player/tizen_player.dart
+++ lib/pages/player/tizen_player.dart
@@ -2,11 +2,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:video_player/video_player.dart';
-import 'package:video_player_tizen/video_player_tizen.dart';
+import 'package:video_player_videohole/video_player.dart';
+import 'package:video_player_videohole/video_player_platform_interface.dart';
 
-import '../../helpers/helpers.dart';
 
 class TizenScreen extends StatefulWidget {
   const TizenScreen({
@@ -29,41 +27,55 @@ class _FullVideoScreenState extends State<TizenScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(
-        widget.link))
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
-      });
+    _controller = VideoPlayerController.network(
+        widget.link);
+    _controller.addListener(() {
+      if (_controller.value.hasError) {
+        print(_controller.value.errorDescription);
+      }
+      setState(() {});
+    });
+    _controller.setLooping(true);
+    _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Video Demo',
-      home: Scaffold(
-        body: Center(
-          child: _controller.value.isInitialized
-              ? AspectRatio(
+    return Scaffold(
+        body: _controller.value.isInitialized
+            ? AspectRatio(
             aspectRatio: _controller.value.aspectRatio,
-            child: VideoPlayer(_controller),
-          )
-              : Container(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
-        ),
-      ),
+            child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(child: VideoPlayer(_controller)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: MaterialButton(
+                              onPressed: () {
+                                setState(() {
+                                  _controller.value.isPlaying
+                                      ? _controller.pause()
+                                      : _controller.play();
+                                });
+                              },
+                              child: Icon(size: 30, color: Colors.white,
+                                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                              )
+                          )),
+                      _GetTextTrackButton(controller: _controller),
+                    ],
+                  ),
+                  ClosedCaption(text: _controller.value.caption.text),
+                  VideoProgressIndicator(_controller, allowScrubbing: true),
+                ]))
+            : Container()
     );
+
   }
 
   @override
@@ -72,3 +84,46 @@ class _FullVideoScreenState extends State<TizenScreen> {
     super.dispose();
   }
 }
+class _GetTextTrackButton extends StatelessWidget {
+  const _GetTextTrackButton({required this.controller});
+
+  final VideoPlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: MaterialButton(
+          child: const Text('Sub language', style: TextStyle(fontSize:20,
+            color: Colors.white,
+          ),),
+          onPressed: () async {
+            final List<TextTrack>? textTracks = await controller.textTracks;
+            if (textTracks == null) {
+              return;
+            }
+            await showDialog<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Select language:'),
+                    content: SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: ListView.builder(
+                          itemCount: textTracks.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              title: Text(
+                                  'language:${textTracks[index].language}'),
+                              onTap: () {
+                                controller.setTrackSelection(textTracks[index]);
+                              },
+                            );
+                          },
+                        )),
+                  );
+                });
+          }),
+    );
+  }}
